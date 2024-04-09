0966cce4425d1c48ab847b763bc0b2004a20ba79
xxhyxxnjxn <hyunjin077@naver.com>
2024-04-09 15:00:27 +0900

    path parameters 추가

--- src/test/java/com/example/demo/controller/RestDocsTest.java
+++ src/test/java/com/example/demo/controller/RestDocsTest.java
@@ -1,17 +1,32 @@
 package com.example.demo.controller;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.request.RequestDocumentation;
 
 @WebMvcTest(RestDocsTestController.class)
 public class RestDocsTest extends AbstractRestDocsTest {
 	@Test
 	void RestDocsTest() throws Exception {
 		mockMvc.perform(get("/restDocsTest")).andExpect(status().isOk());
+	}
+
+	@Test
+	void RestDocsTestById() throws Exception {
+		mockMvc.perform(RestDocumentationRequestBuilders.get("/restDocsTest/{id}","test"))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andDo(document("restDocsTest-get",
+				RequestDocumentation.pathParameters(
+						RequestDocumentation.parameterWithName("id").description("test id")
+				)
+			));
 	}
 
 	@Test
