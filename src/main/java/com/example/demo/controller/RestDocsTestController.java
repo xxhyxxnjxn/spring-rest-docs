622dec6f5840bab012d3a85a6501567a1789f8f2
정현진 <hyeonjin0905@inzent.com>
2024-04-09 15:00:27 +0900

    path parameters 추가

--- src/main/java/com/example/demo/controller/RestDocsTestController.java
+++ src/main/java/com/example/demo/controller/RestDocsTestController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,7 +11,12 @@ public class RestDocsTestController {
 	  public String restDocsTestAPI() {
 	    return "test!!";
 	  }
-	  
+
+	  @GetMapping("/restDocsTest/{id}")
+	  public String restDocsTestAPIById(@PathVariable("id") String id) {
+	    return id;
+	  }
+
 	  @GetMapping("/restDocsTest2")
 	  public String restDocsTestAPI2() {
 	    return "test2!!";
